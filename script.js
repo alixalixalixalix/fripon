109b3282940c3ab13d67a6a9f992da8592612afe
Alix Bocquier <alixbocqui@gmail.com>
2025-09-24 17:49:39 +0200

    modal + hover ok

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const fetchData = async () => {
     function generatorServices(dataServices) {
       const services__liste = document.querySelector("#services__liste div");
 
+      const allButtons = [];
       // GÉNÉRATION SERVICES
       for (let i = 0; i < dataServices.length; i++) {
         const services__card = document.createElement("div");
@@ -30,7 +31,8 @@ const fetchData = async () => {
 
         const button = document.createElement("button");
         services__card__txt.appendChild(button);
-        button.innerText = "En savoir plus";
+        button.innerText = "En savoir plus"; // ICI
+        allButtons.push(button);
 
         const services__card__tarifs = document.createElement("div");
         services__card__tarifs.classList.add("services__card__tarifs");
@@ -44,84 +46,118 @@ const fetchData = async () => {
         services__card__tarifs.appendChild(duree);
         duree.innerText = dataServices[i].duree;
       }
-    }
 
+      allButtons.forEach((button, index) => {
+        button.addEventListener("click", (event) => {
+          const modal = document.getElementById("modal__liste");
+          modal.style.display = "flex";
+
+          const close = document.querySelector(".modal__card__intro img");
+          close.addEventListener("click", (event) => {
+            modal.style.display = "none";
+          });
+
+          const modal__card__intro = document.querySelector(
+            ".modal__card__intro h2"
+          );
+          modal__card__intro.innerText = dataServices[index].titre;
+
+          // POUR QUI
+          const modal__card__paragraphe1 = document.querySelector(
+            ".modal__card__paragraphe1"
+          );
+          const modal__card__paragraphe1__titre = document.querySelector(
+            ".modal__card__paragraphe1 div h3"
+          );
+          modal__card__paragraphe1__titre.innerText =
+            dataServices[index].titreParagraphe1;
+          const listeUl__1 = document.querySelector(
+            ".modal__card__paragraphe1 ul"
+          );
+          listeUl__1.innerHTML = "";
+
+          for (
+            let j = 0;
+            j < dataServices[index].contentParagraphe1.length;
+            j++
+          ) {
+            const li = document.createElement("li");
+            listeUl__1.appendChild(li);
+            li.innerText = dataServices[index].contentParagraphe1[j];
+          }
+
+          // OBJECTIFS
+          const modal__card__paragraphe2 = document.querySelector(
+            ".modal__card__paragraphe2"
+          );
+          const modal__card__paragraphe2__titre = document.querySelector(
+            ".modal__card__paragraphe2 div h3"
+          );
+          modal__card__paragraphe2__titre.innerText =
+            dataServices[index].titreParagraphe2;
+
+          const listeUl__2 = document.querySelector(
+            ".modal__card__paragraphe2 ul"
+          );
+          listeUl__2.innerHTML = "";
+
+          for (
+            let j = 0;
+            j < dataServices[index].contentParagraphe2.length;
+            j++
+          ) {
+            const li = document.createElement("li");
+            listeUl__2.appendChild(li);
+            li.innerText = dataServices[index].contentParagraphe2[j];
+          }
+
+          // COMMENT ÇA MARCHE
+          const modal__card__paragraphe3 = document.querySelector(
+            ".modal__card__paragraphe3"
+          );
+          const modal__card__paragraphe3__titre = document.querySelector(
+            ".modal__card__paragraphe3 div h3"
+          );
+          modal__card__paragraphe3__titre.innerText =
+            dataServices[index].titreParagraphe3;
+          const listeUl__3 = document.querySelector(
+            ".modal__card__paragraphe3 ul"
+          );
+          listeUl__3.innerHTML = "";
+
+          for (
+            let j = 0;
+            j < dataServices[index].contentParagraphe3.length;
+            j++
+          ) {
+            const li = document.createElement("li");
+            listeUl__3.appendChild(li);
+            li.innerText = dataServices[index].contentParagraphe3[j];
+          }
+
+          // LOCALISATION
+          const modal__card__paragraphe4 = document.querySelector(
+            ".modal__card__paragraphe4"
+          );
+          const modal__card__paragraphe4__titre = document.querySelector(
+            ".modal__card__paragraphe4 div h3"
+          );
+          modal__card__paragraphe4__titre.innerText =
+            dataServices[index].titreParagraphe4;
+          const pLocalisation = document.querySelector(
+            ".modal__card__paragraphe4 p"
+          );
+          pLocalisation.innerText = dataServices[index].contentParagraphe4;
+        });
+      });
+    }
 
     // GÉNÉRATION MODAL
 
     // On garde la structure dans le html
+    // On récupère dans un tableau les 4 boutons
     // Au clic sur un "en savoir plus", on regarde quel bouton a été cliqué
     // On maj les infos dans chaque élément html
-
-    const modal__card__intro = document.querySelector(".modal__card__intro h2");
-    modal__card__intro.innerText = dataServices[3].titre;
-
-    // POUR QUI
-    const modal__card__paragraphe1 = document.querySelector(
-      ".modal__card__paragraphe1"
-    );
-    const modal__card__paragraphe1__titre = document.querySelector(
-      ".modal__card__paragraphe1 div h3"
-    );
-    modal__card__paragraphe1__titre.innerText = dataServices[3].titreParagraphe1;
-    const listeUl__1 = document.createElement("ul");
-    modal__card__paragraphe1.appendChild(listeUl__1);
-
-    for (let j = 0; j < dataServices[3].contentParagraphe1.length; j++) {
-      const li = document.createElement("li");
-      listeUl__1.appendChild(li);
-      li.innerText = dataServices[3].contentParagraphe1[j];
-    }
-
-    // OBJECTIFS
-    const modal__card__paragraphe2 = document.querySelector(
-      ".modal__card__paragraphe2"
-    );
-    const modal__card__paragraphe2__titre = document.querySelector(
-      ".modal__card__paragraphe2 div h3"
-    );
-    modal__card__paragraphe2__titre.innerText = dataServices[3].titreParagraphe2;
-
-    const listeUl__2 = document.createElement("ul");
-    modal__card__paragraphe2.appendChild(listeUl__2);
-
-    for (let j = 0; j < dataServices[3].contentParagraphe2.length; j++) {
-      const li = document.createElement("li");
-      listeUl__2.appendChild(li);
-      li.innerText = dataServices[3].contentParagraphe2[j];
-    }
-
-    // COMMENT ÇA MARCHE
-    const modal__card__paragraphe3 = document.querySelector(
-      ".modal__card__paragraphe3"
-    );
-    const modal__card__paragraphe3__titre = document.querySelector(
-      ".modal__card__paragraphe3 div h3"
-    );
-    modal__card__paragraphe3__titre.innerText = dataServices[3].titreParagraphe3;
-    const listeUl__3 = document.createElement("ul");
-    modal__card__paragraphe3.appendChild(listeUl__3);
-
-    for (let j = 0; j < dataServices[3].contentParagraphe3.length; j++) {
-      const li = document.createElement("li");
-      listeUl__3.appendChild(li);
-      li.innerText = dataServices[3].contentParagraphe3[j];
-    }
-
-    // LOCALISATION
-    const modal__card__paragraphe4 = document.querySelector(
-      ".modal__card__paragraphe4"
-    );
-    const modal__card__paragraphe4__titre = document.querySelector(
-      ".modal__card__paragraphe4 div h3"
-    );
-    modal__card__paragraphe4__titre.innerText = dataServices[3].titreParagraphe4;
-    const pLocalisation = document.querySelector(
-      ".modal__card__paragraphe4 p"
-    );
-    pLocalisation.innerText = dataServices[3].contentParagraphe4
-    ;
-
 
     /*
     function generatorModal(dataServices) {
